8714e5b45de0b35caf9485a6c7e8d6b95d0a83b8
NutonFlash <my.mr6@mail.ru>
2023-01-25 15:39:33 +0600

    fix some XSS vulnerabilities

--- course/php/requestHandler.php
+++ course/php/requestHandler.php
@@ -8,10 +8,10 @@ $relativePath = '../views/';
 $isAuth = $_COOKIE['isAuth'];
 $hasAccess = AuthUtilities::hasAccess();
 $hash = htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES);
-$paths = explode('/', $_SERVER['QUERY_STRING'], 2);
+$paths = explode('/', $hash, 2);
 $login = null;
 $email = null;
-$isFirstLoad = preg_match('#.+&isFirstLoad#', $_SERVER['QUERY_STRING']);
+$isFirstLoad = preg_match('#.+&isFirstLoad#', $hash);
 $isModule1 = preg_match('#module1#', $hash);
 $isModule3 = preg_match('#module3#', $hash);
 $isModule5 = preg_match('#module5#', $hash);
@@ -20,6 +20,9 @@ $modal = null;
 $sidebar = null;
 $navbar = null;
 $content = null;
+$module1 = htmlspecialchars($_COOKIE['module1'], ENT_QUOTES);
+$module3 = htmlspecialchars($_COOKIE['module3'], ENT_QUOTES);
+$module5 = htmlspecialchars($_COOKIE['module5'], ENT_QUOTES);
 
 if ($isAuth === 'true') {
     $login = $_COOKIE['login'];
@@ -27,19 +30,19 @@ if ($isAuth === 'true') {
 }
 
 if ($isFirstLoad) {
-    $hash = explode('&', $_SERVER['QUERY_STRING'])[0];
+    $hash = explode('&', $hash)[0];
     $paths = explode('/', $hash, 2);
 }
 
 if (preg_match('#module1/.+|module3/.+|module5/.+#', $hash)) {
     if ($paths[0] === 'module1') {
-        setcookie('module1', $_COOKIE['module1'], time() - 3600, '/');
+        setcookie('module1', $module1, time() - 3600, '/');
         setcookie('module1', $paths[1], time() + 60 * 60 * 24 * 14, '/');
     } else if ($paths[0] === 'module3') {
-        setcookie('module3', $_COOKIE['module3'], time() - 3600, '/');
+        setcookie('module3', $module3, time() - 3600, '/');
         setcookie('module3', $paths[1], time() + 60 * 60 * 24 * 14, '/');
     } else {
-        setcookie('module5', $_COOKIE['module5'], time() - 3600, '/');
+        setcookie('module5', $module5, time() - 3600, '/');
         setcookie('module5', $paths[1], time() + 60 * 60 * 24 * 14, '/');
     }
     include_once($relativePath . 'utilities/sidebar.html'); // sidebar
@@ -47,11 +50,11 @@ if (preg_match('#module1/.+|module3/.+|module5/.+#', $hash)) {
 } else if (preg_match('#module1|module3|module5#', $hash)) {
     include_once($relativePath . 'utilities/sidebar.html'); // sidebar
     if ($hash === 'module1') {
-        include_once($relativePath . $hash . '/' . $_COOKIE['module1'] . '.html'); // content
+        include_once($relativePath . $hash . '/' . $module1 . '.html'); // content
     } else if ($hash === 'module3'){
-        include_once($relativePath . $hash . '/' . $_COOKIE['module3'] . '.html'); // content
+        include_once($relativePath . $hash . '/' . $module3 . '.html'); // content
     } else {
-        include_once($relativePath . $hash . '/' . $_COOKIE['module5'] . '.html'); // content
+        include_once($relativePath . $hash . '/' . $module5 . '.html'); // content
     }
 } else {
     include_once($relativePath . $hash . '.html'); //content
